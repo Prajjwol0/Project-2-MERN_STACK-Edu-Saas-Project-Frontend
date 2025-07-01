87f351913ddc2f4ba886d0c992fe4c440c9c6155
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-07-01 17:59:08 +0545

    Initialization

---
+++ src/app/about/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+function AboutId(){
+    return(
+        <>
+        <h1>Hello World</h1>
+        </>
+    )
+}
+
+export default AboutId
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+
+
+function Page(){
+   return(
+     <>
+    <h1>This is contact page</h1>
+    </>
+   )
+}
+
+export default Page;
