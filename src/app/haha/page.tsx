630017c2ed855020b3be07ce3f612f8bd932ef29
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-07-06 19:51:42 +0545

    push

--- src/app/haha/page.tsx
+++ src/app/haha/page.tsx
@@ -1,12 +1,12 @@
 
 
-import { useSelector } from "react-redux"
+import { useAppSelector } from "@/lib/store/hooks"
 
 
 
 function Haha(){
 
-    useSelector((store)=>store.teacher)
+const{teacherName,teacherPassword}=useAppSelector((store)=>store.teacher)
     console.log(data.teacherName,data.teacherPassword)
     return(
         <h1>hahahahaha</h1>
